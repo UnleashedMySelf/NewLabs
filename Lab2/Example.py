07ce6227ad27328e3cd0b6630a31e4ecd114563b
UnleashedMySelf <unleashedmyself@gmail.com>
2017-02-21 15:35:12 +0200

    Update Example.py

--- Lab2/Example.py
+++ Lab2/Example.py
@@ -4,4 +4,34 @@ Class Card:
     Self.suit = suit
 Class Table:
   Class Deck:
-    
+    def shuffle(self):
+        n = len(self.cards)
+        newcards = []
+        for i in randperm(n):
+            newcards.append(self.cards[i])
+        self.cards = newcards
+
+    def userclickhandler(self):
+        opendeck = self.game.opendeck
+        card = self.deal()
+        if not card:
+            while 1:
+                card = opendeck.deal()
+                if not card:
+                    break
+                self.add(card)
+                card.showback()
+        else:
+            self.game.opendeck.add(card)
+            card.showface()
+
+
+    def randperm(n):
+    """Function returning a random permutation of range(n)."""
+    r = range(n)
+    x = []
+    while r:
+        i = random.choice(r)
+        x.append(i)
+        r.remove(i)
+    return x
